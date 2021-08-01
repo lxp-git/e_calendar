969792b5d8089bcb515be287f3b8865c104cb79a
xuanping <a845326948@gmail.com>
2021-08-01 14:37:43 +0800

    fix: bugs

--- src/pages/index/FloatButton/index.tsx
+++ src/pages/index/FloatButton/index.tsx
@@ -15,9 +15,11 @@ const styles = StyleSheet.create({
     width: Taro.pxTransform(120),
     height: Taro.pxTransform(120),
     borderRadius: Taro.pxTransform(120),
+    background: 'transparent',
     display: "flex",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    boxShadow: '0 6px 10px -2px rgba(0, 0, 0, 0.2), 0 12px 20px 0 rgba(0, 0, 0, 0.14), 0 2px 36px 0 rgba(0, 0, 0, 0.12)',
   },
   image: {
     width: Taro.pxTransform(54),
